164ec215e94d10b5c91d1e3382f13a4eed112147
jamshid <shokirovjamshid555@gmail.com>
2026-02-16 16:34:20 +0500

    change models

--- root/urls.py
+++ root/urls.py
@@ -1,11 +1,15 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
+from root import settings
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include('apps.urls')),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('i18n/', include('django.conf.urls.i18n')),
-]
+                  path('admin/', admin.site.urls),
+                  path('api/v1/', include('apps.urls')),
+                  path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+                  path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+                  path('i18n/', include('django.conf.urls.i18n')),
+                  path("ckeditor5/", include('django_ckeditor_5.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
